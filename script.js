26e1a322b88b27de8b179ba19216e778319d3687
mr-iconic <90669066+mr-iconic@users.noreply.github.com>
2022-03-10 11:30:46 +0530

    commit

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// function onclickmenu(){
 
-//     document.getElementById("menu").classList.toggle("hem");
-//     document.getElementById("item").classList.toggle("change");
-// }
 
 function onclickmenu() {
     var x=document.getElementById("item");
@@ -10,5 +6,17 @@ function onclickmenu() {
         x.style.display="none";
     }else{
         x.style.display="block";
+    }
+}
+
+function onclick1() {
+    var x=document.getElementById("form");
+    var y=document.getElementById("form2");
+    if(x.style.display==="block"){
+        x.style.display="none";
+        y.style.display="block";
+    }else{
+        x.style.display="block";
+        y.style.display="none";
     }
 }
